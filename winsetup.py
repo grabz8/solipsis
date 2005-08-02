b87787dcf5c797a5283da6c8f16c16b18a54755a
Didier G. (didierg) <vespertinian@hotmail.com>
2005-08-02 13:27:12 +0000

    release with profile filters
    git-svn-id: svn://svn.berlios.de/solipsis/branches/release-0.9.2@660 86f5558a-632e-8943-aa3e-3f3233a3a6cb

--- winsetup.py
+++ winsetup.py
@@ -13,7 +13,7 @@ Edit this script to insert desired services (see data_files)
 Then  issue the command $python setup.py py2exe
 """
 name="Solipsis"
-version="0.9"
+version="0.9.2"
 description="Solipsis, A peer-to-peer system for a massively multi-participant virtual world"
 author="France Telecom R&D"
 author_email="solipsis-tech@lists.berlios.de"
@@ -37,13 +37,6 @@ data_files=[
     ("conf", ["conf/solipsis.conf"]),
 # Insert services files here
     ("solipsis/services", glob.glob("solipsis/services/*.*")),
-#CHAT
-    ("solipsis/services/chat", glob.glob("solipsis/services/chat/*.*")),
-    ("solipsis/services/chat/po", glob.glob("solipsis/services/chat/po/*.*")),
-    ("solipsis/services/chat/po/de/LC_MESSAGES", 
-        glob.glob("solipsis/services/chat/po/de/LC_MESSAGES/*.*")),
-    ("solipsis/services/chat/po/fr/LC_MESSAGES", 
-        glob.glob("solipsis/services/chat/po/fr/LC_MESSAGES/*.*")),
 #AVATARS
     ("avatars", glob.glob("avatars/*.*")),
     ("solipsis/services/avatar", glob.glob("solipsis/services/avatar/*.*")),
@@ -52,20 +45,35 @@ data_files=[
         glob.glob("solipsis/services/avatar/po/de/LC_MESSAGES/*.*")),
     ("solipsis/services/avatar/po/fr/LC_MESSAGES", 
         glob.glob("solipsis/services/avatar/po/fr/LC_MESSAGES/*.*")),
+    ("solipsis/services/avatar/po/ja/LC_MESSAGES", 
+        glob.glob("solipsis/services/avatar/po/ja/LC_MESSAGES/*.*")),
+    ("solipsis/services/avatar/po/sl/LC_MESSAGES", 
+        glob.glob("solipsis/services/avatar/po/sl/LC_MESSAGES/*.*")),
+#CHAT
+    ("solipsis/services/chat", glob.glob("solipsis/services/chat/*.*")),
+    ("solipsis/services/chat/po", glob.glob("solipsis/services/chat/po/*.*")),
+    ("solipsis/services/chat/po/de/LC_MESSAGES", 
+        glob.glob("solipsis/services/chat/po/de/LC_MESSAGES/*.*")),
+    ("solipsis/services/chat/po/fr/LC_MESSAGES", 
+        glob.glob("solipsis/services/chat/po/fr/LC_MESSAGES/*.*")),
+    ("solipsis/services/chat/po/ja/LC_MESSAGES", 
+        glob.glob("solipsis/services/chat/po/ja/LC_MESSAGES/*.*")),
+    ("solipsis/services/chat/po/sl/LC_MESSAGES", 
+        glob.glob("solipsis/services/chat/po/sl/LC_MESSAGES/*.*")),
 #PROFILE
     ("solipsis/services/profile", glob.glob("solipsis/services/profile/*.*")),
-    ("solipsis/services/profile/tests", glob.glob("solipsis/services/profile/tests/*.*")),
-    ("solipsis/services/profile/tests/data", glob.glob("solipsis/services/profile/tests/data/*.*")),
-    ("solipsis/services/profile/tests/data/profiles", glob.glob("solipsis/services/profile/tests/data/profiles/*.*")),
-    ("solipsis/services/profile/tests/data/subdir1", glob.glob("solipsis/services/profile/tests/data/subdir1/*.*")),
-    ("solipsis/services/profile/tests/data/subdir1/subsubdir", glob.glob("solipsis/services/profile/tests/data/subdir1/subsubdir/*.*")),
-    ("solipsis/services/profile/tests/data/emptydir", glob.glob("solipsis/services/profile/tests/data/emptydir/*.*")),
-    ("solipsis/services/profile/tests/generated", glob.glob("solipsis/services/profile/tests/generated/*.*")),
     ("solipsis/services/profile/gui", glob.glob("solipsis/services/profile/gui/*.*")),
     ("solipsis/services/profile/images", glob.glob("solipsis/services/profile/images/*.*")),
     ("solipsis/services/profile/po", glob.glob("solipsis/services/profile/po/*.*")),
     ("solipsis/services/profile/po/fr", glob.glob("solipsis/services/profile/po/fr/*.*")),
     ("solipsis/services/profile/po/fr/LC_MESSAGES", glob.glob("solipsis/services/profile/po/fr/LC_MESSAGES/*.*")),
+    ("solipsis/services/profile/tests", glob.glob("solipsis/services/profile/tests/*.*")),
+    ("solipsis/services/profile/tests/data", glob.glob("solipsis/services/profile/tests/data/*.*")),
+    ("solipsis/services/profile/tests/data/emptydir", glob.glob("solipsis/services/profile/tests/data/emptydir/*.*")),
+    ("solipsis/services/profile/tests/data/profiles", glob.glob("solipsis/services/profile/tests/data/profiles/*.*")),
+    ("solipsis/services/profile/tests/data/subdir1", glob.glob("solipsis/services/profile/tests/data/subdir1/*.*")),
+    ("solipsis/services/profile/tests/data/subdir1/subsubdir", glob.glob("solipsis/services/profile/tests/data/subdir1/subsubdir/*.*")),
+    ("solipsis/services/profile/tests/generated", glob.glob("solipsis/services/profile/tests/generated/*.*")),
 # End services files
     ("log", []),
     ("state", []),
@@ -76,7 +84,13 @@ data_files=[
         "po/de/LC_MESSAGES/solipsis.po"]),
     ("po/fr/LC_MESSAGES", 
         ["po/fr/LC_MESSAGES/solipsis.mo",
-        "po/fr/LC_MESSAGES/solipsis.po"])]
+        "po/fr/LC_MESSAGES/solipsis.po"]),
+    ("po/ja/LC_MESSAGES", 
+        ["po/ja/LC_MESSAGES/solipsis.mo",
+        "po/ja/LC_MESSAGES/solipsis.po"]),
+    ("po/sl/LC_MESSAGES", 
+        ["po/sl/LC_MESSAGES/solipsis.mo",
+        "po/sl/LC_MESSAGES/solipsis.po"])]
 
 import py2exe
 
