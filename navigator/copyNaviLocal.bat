45a991bfbdf94fa4bd5619743f412aad809150e0
Jangre (jangre) <>
2007-09-24 07:54:30 +0000

    Solipsis global solution added
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@21 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- navigator/copyNaviLocal.bat
+++ navigator/copyNaviLocal.bat
@@ -1,15 +1,15 @@
 @echo off
 
-if exist bin\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.html bin\Debug\NaviLocal /s /y
-if exist bin\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.css bin\Debug\NaviLocal /s /y
-if exist bin\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.js bin\Debug\NaviLocal /s /y
-if exist bin\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.png bin\Debug\NaviLocal /s /y
-if exist bin\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.txt bin\Debug\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.html ..\Common\bin\navigator\Debug\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.css ..\Common\bin\navigator\Debug\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.js ..\Common\bin\navigator\Debug\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.png ..\Common\bin\navigator\Debug\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.txt ..\Common\bin\navigator\Debug\NaviLocal /s /y
 
-if exist bin\Release\NaviLocal xcopy ..\Media\NaviLocal\*.html bin\Release\NaviLocal /s /y
-if exist bin\Release\NaviLocal xcopy ..\Media\NaviLocal\*.css bin\Release\NaviLocal /s /y
-if exist bin\Release\NaviLocal xcopy ..\Media\NaviLocal\*.js bin\Release\NaviLocal /s /y
-if exist bin\Release\NaviLocal xcopy ..\Media\NaviLocal\*.png bin\Release\NaviLocal /s /y
-if exist bin\Release\NaviLocal xcopy ..\Media\NaviLocal\*.txt bin\Release\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.html ..\Common\bin\navigator\Release\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.css ..\Common\bin\navigator\Release\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.js ..\Common\bin\navigator\Release\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.png ..\Common\bin\navigator\Release\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.txt ..\Common\bin\navigator\Release\NaviLocal /s /y
 
 pause
