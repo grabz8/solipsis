44fc92ca5c5e55c40d26613ef87db188130ed97d
Jangre (jangre) <>
2007-09-27 09:55:11 +0000

    prefer Common/bin
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@66 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- MediaTools/OgreMeshesUpgrade.bat
+++ MediaTools/OgreMeshesUpgrade.bat
@@ -1,8 +1,12 @@
 @echo off
+rem 
+rem This tool is usefull when you want to upgrade all mesh files of 1 directory
+rem just drag and drop the directory you want to convert mesh files on this .BAT command batch
+rem
 rem OGRE_HOME must be defined (eg. c:\Work\ogre144src\ogrenew)
-rem OgreMeshUpgrade.exe must be built and present into %OGRE_HOME%\Tools\MeshUpgrader\bin\debug\OgreMeshUpgrade.exe
+rem OgreMeshUpgrade.exe must be built and present into %OGRE_HOME%\Tools\Common\bin\release\OgreMeshUpgrade.exe
 
 PATH=%PATH%;%OGRE_HOME%\lib
 cd %1
-for %%f in (*.mesh) do %OGRE_HOME%\Tools\MeshUpgrader\bin\debug\OgreMeshUpgrade.exe %%f
+for %%f in (*.mesh) do %OGRE_HOME%\Tools\Common\bin\release\OgreMeshUpgrade.exe %%f
 pause
