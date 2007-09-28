d7aab1ef368c303c0d78432498fd62d0bc793cea
Jangre (jangre) <>
2007-09-28 11:53:13 +0000

    Comments added
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@70 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- MediaTools/lexiExporterSet.bat
+++ MediaTools/lexiExporterSet.bat
@@ -1,3 +1,9 @@
+rem
+rem Copy this batch into your 3DS Max directory
+rem Install lexiExporter plugin and run lexiExporterUnSet.bat to create backup files
+rem Then call Set to restore plugin and UnSet to remove it
+rem
+
 pause
 copy lexiExporter_cg.dll cg.dll /y
 copy lexiExporter_OgreMain.dll OgreMain.dll /y
--- MediaTools/oFusionSet.bat
+++ MediaTools/oFusionSet.bat
@@ -1,3 +1,9 @@
+rem
+rem Copy this batch into your 3DS Max directory
+rem Install oFusion plugin and run oFusionUnSet.bat to create backup files
+rem Then call Set to restore plugin and UnSet to remove it
+rem
+
 pause
 copy oFusion_cg.dll cg.dll /y
 copy oFusion_OgreMain.dll OgreMain.dll /y
