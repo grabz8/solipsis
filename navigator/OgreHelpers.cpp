157288b412ee789378615d23aeb52729eb90c08e
Jangre (jangre) <>
2007-10-03 15:48:17 +0000

    OgreHelpers added
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@95 53240bf5-9d7a-4d4b-845a-3b2958f72d47

---
+++ navigator/OgreHelpers.cpp
@@ -0,0 +1,65 @@
+#include "OgreHelpers.h"
+
+void OgreHelpers::getMovableObjectsList(SceneNode* node, const String movableType, std::list<MovableObject*> &movableObjectsList)
+{
+    SceneNode::ObjectIterator objectIterator = node->getAttachedObjectIterator();
+    while (objectIterator.hasMoreElements())
+    {
+        MovableObject* movableObject = objectIterator.getNext();
+        if (movableObject->getMovableType().compare(movableType) == 0)
+            movableObjectsList.push_back(movableObject);
+    }
+    Node::ChildNodeIterator childNodeIterator = node->getChildIterator();
+    while (childNodeIterator.hasMoreElements())
+    {
+        Node* childNode = childNodeIterator.getNext();
+        getMovableObjectsList((SceneNode*)childNode, movableType, movableObjectsList);
+    }
+}
+
+bool OgreHelpers::convertRealString(const String& real, Real& r)
+{
+    Real result = 0;
+
+    if (1 != sscanf(real.c_str(), "%f", &result))
+        return false;
+    r = result;
+
+    return true;
+}
+
+bool OgreHelpers::convertVector3String(const String& vector, Vector3& v)
+{
+    Vector3 result = Vector3::ZERO;
+    size_t prev, comma;
+
+    comma = vector.find_first_of("(", 0);
+    if (comma == String::npos)
+    {
+        comma = -1;
+        prev = 0;
+    }
+    else
+    {
+        comma += 1;
+        prev = comma;
+    }
+    comma = vector.find_first_of(",", comma + 1);
+    if (comma == String::npos) return false;
+    if (1 != sscanf(vector.substr(prev, comma).c_str(), "%f", &result.x))
+        return false;
+    prev = comma + 1;
+    comma = vector.find_first_of(",", comma + 1);
+    if (comma == String::npos) return false;
+    if (1 != sscanf(vector.substr(prev, comma).c_str(), "%f", &result.y))
+        return false;
+    prev = comma + 1;
+    comma = vector.find_first_of(")", comma + 1);
+    if (comma != String::npos)
+        comma -= 1;
+    if (1 != sscanf(vector.substr(prev, comma).c_str(), "%f", &result.z))
+        return false;
+    v = result;
+
+    return true;
+}
---
+++ navigator/OgreHelpers.h
@@ -0,0 +1,24 @@
+#ifndef __OgreHelpers_h__
+#define __OgreHelpers_h__
+
+#include "Ogre.h"
+
+using namespace Ogre;
+
+class OgreHelpers
+{
+private:
+    // No instanciation
+    OgreHelpers() {};
+
+public:
+    // Retrieve recursivelly from 1 scene node all movable objects of 1 type
+    static void getMovableObjectsList(SceneNode* node, const String movableType, std::list<MovableObject*> &movableObjectsList);
+
+    // Convert 1 string representation of Real into 1 Real
+    static bool convertRealString(const String& real, Real& r);
+    // Convert 1 string representation of Vector3 (ie. x, y, z) into 1 Vector3
+    static bool convertVector3String(const String& vector, Vector3& v);
+};
+
+#endif // #ifndef __OgreHelpers_h__
