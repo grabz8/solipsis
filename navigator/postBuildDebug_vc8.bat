cf7fdcad21522b21d94ddf24a5fa5cfe71342762
Jangre (jangre) <>
2007-10-08 12:08:50 +0000

    now OgreOde.dll is using static ode.lib instead of ode.dll
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@116 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- navigator/postBuildDebug_vc8.bat
+++ navigator/postBuildDebug_vc8.bat
@@ -11,7 +11,6 @@ if not exist "%1\pthreadVC_d.dll" xcopy "..\Dependencies\pthreads\lib\pthreadVC_
 if not exist "%1\xul.dll" xcopy "..\Dependencies\llmozlib\win32_vc8\runtime\Debug\*.*" "%1" /s /y
 if not exist "%1\NaviLocal\" xcopy "..\Media\NaviLocal\*.*" "%1\NaviLocal" /s /y /i
 if not exist "%1\NaviLocal\Navi.js" xcopy "..\Dependencies\navi\Javascript\*.*" "%1\NaviLocal" /y
-if not exist "%1\ode.dll" xcopy "..\Dependencies\ode\lib\debugdll\ode.dll" "%1" /s /y
 if not exist "%1\OgreOde_Core_d.dll" xcopy "..\Dependencies\OgreOde\lib\OgreOde_Core_d.dll" "%1" /s /y
 if not exist "%1\Plugins.cfg" xcopy "%OGRE_HOME%\Samples\Common\bin\Debug\Plugins.cfg" "%1" /s /y
 if not exist "%1\quake3settings.cfg" xcopy "%OGRE_HOME%\Samples\Common\bin\Debug\quake3settings.cfg" "%1" /s /y
