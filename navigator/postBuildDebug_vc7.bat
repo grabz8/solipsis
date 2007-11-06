eb7bd34cd6751c28018cac73abf0c3587f93e342
Jangre (jangre) <>
2007-11-06 10:20:18 +0000

    Lua scripts boot + defaultGUI added with avatar context menu
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@175 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- navigator/postBuildDebug_vc7.bat
+++ navigator/postBuildDebug_vc7.bat
@@ -11,6 +11,7 @@ if not exist "%1\pthreadVC_d.dll" xcopy "..\Dependencies\pthreads\lib\pthreadVC_
 if not exist "%1\xul.dll" xcopy "..\Dependencies\llmozlib\win32_vc7\runtime\Debug\*.*" "%1" /s /y
 if not exist "%1\NaviLocal\" xcopy "..\Media\NaviLocal\*.*" "%1\NaviLocal" /s /y /i
 if not exist "%1\NaviLocal\Navi.js" xcopy "..\Dependencies\navi\Javascript\*.*" "%1\NaviLocal" /y
+if not exist "%1\lua\" xcopy "..\Media\lua\*.*" "%1\lua" /s /y /i
 if not exist "%1\OgreOde_Core_d.dll" xcopy "..\Dependencies\OgreOde\lib\OgreOde_Core_d.dll" "%1" /s /y
 if not exist "%1\Plugins.cfg" xcopy "%OGRE_HOME%\Samples\Common\bin\Debug\Plugins.cfg" "%1" /s /y
 if not exist "%1\quake3settings.cfg" xcopy "%OGRE_HOME%\Samples\Common\bin\Debug\quake3settings.cfg" "%1" /s /y
