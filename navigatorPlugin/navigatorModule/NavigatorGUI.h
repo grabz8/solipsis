7185415651f1de1ab4ed2879c100bd8fecaaa021
Jangre (jangre) <>
2007-11-15 17:01:30 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@225 53240bf5-9d7a-4d4b-845a-3b2958f72d47

---
+++ navigatorPlugin/navigatorModule/NavigatorGUI.h
@@ -0,0 +1,93 @@
+#ifndef __NavigatorGUI_h__
+#define __NavigatorGUI_h__
+
+#include "NaviManager.h"
+
+using namespace NaviLibrary;
+
+class Navigator;
+
+class NavigatorGUI
+{
+public:
+    enum NaviState {
+        NSNotCreated,   // Navi page not created
+        NSCreated       // Navi page created and not visible
+    };
+
+    enum NaviPanel {
+        NAVI_LOGIN = 0,
+        NAVI_OPTIONS,
+        NAVI_CHAT,
+        NAVI_CONTEXT,
+        NAVI_MODELERMAIN,
+#ifdef UIDEBUG
+        NAVI_DEBUG,
+#endif
+        NAVI_COUNT
+    };
+
+protected:
+    Navigator* mNavigator;
+    NaviManager& mNaviMgr;
+    static const std::string mNavisNames[NAVI_COUNT];
+    NaviState mNavisStates[NAVI_COUNT];
+    int mCurrentNavi;
+
+public:
+    NavigatorGUI(Navigator* navigator);
+    ~NavigatorGUI();
+
+    // Start up GUI
+    bool startup();
+
+    // Mouse
+    void SetMouseVisibility(bool visible);
+    bool isMouseVisible();
+
+    // Interfaces
+    void login();
+    void inWorld();
+
+    // Contextual panel
+    void contextShow(int x, int y, const std::string& items);
+    bool isContextVisible();
+    void contextHide();
+
+    // Main modeler panel
+    void modelerMainShow();
+    bool isModelerMainVisible();
+    void modelerMainHide();
+    void modelerMainUnload();
+
+#ifdef UIDEBUG
+    void switchDebug();
+#endif
+
+protected:
+    // Handlers
+    // Login/Options/InWorld callbacks
+    void loginPageLoaded(const NaviData& naviData);
+    void connect(const NaviData& naviData);
+    void options(const NaviData& naviData);
+    void optionsPageLoaded(const NaviData& naviData);
+    void quit(const NaviData& naviData);
+    void optionsOk(const NaviData& naviData);
+    void optionsBack(const NaviData& naviData);
+    void chatPageLoaded(const NaviData& naviData);
+
+    // Modeler callbacks
+    void modelerMainFileExit(const NaviData& naviData);
+
+#ifdef UIDEBUG
+    // Debug callbacks
+    void debugCommand(const NaviData& naviData);
+#endif
+
+    // Helpers
+    NaviPanel getNaviPanel(const std::string& naviName);
+    void naviToShowPageLoaded(const NaviData& naviData);
+    void hidePreviousNavi();
+};
+
+#endif // #ifndef __NavigatorGUI_h__
