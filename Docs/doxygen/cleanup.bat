5cefb8f2bdda3fa1b7c499c0260b0b900b7bef41
Jangre (jangre) <>
2007-11-27 08:35:27 +0000

    new scale (unit=1 meter) applied into media contents, temporary avatars' positions and physics engines doxygen added
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@265 53240bf5-9d7a-4d4b-845a-3b2958f72d47

---
+++ Docs/doxygen/cleanup.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo ========== Cleaning up... ==========
+
+del html /S /Q
+rmdir html /S /Q
+
+echo ============== Done! ===============
+pause
---
+++ Docs/doxygen/doxygen.bat
@@ -0,0 +1,5 @@
+@echo off
+
+%DOXYGEN_PATH%\doxygen.exe Doxyfile4navigator
+
+pause
