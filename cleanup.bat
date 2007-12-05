7ccc62c6c905c4f5b89b3d404cc664f5815ed5e9
Jangre (jangre) <>
2007-12-05 16:36:52 +0000

    NavigatorModule added NavigatorPlugin and NavigatorActiveX updated now source code is shared by 3 application types and fully contained into NavigatorModule in standAlone application, the AutoCreatedWindow class keep the possibility to use the native Ogre config management / window creation doxygen on NavigatorModule added
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@289 53240bf5-9d7a-4d4b-845a-3b2958f72d47

---
+++ cleanup.bat
@@ -0,0 +1,84 @@
+@echo off
+
+echo This will clean up all dependencies, binaries, objects and libraries.
+echo ** You must close Visual C++ before running this file! **
+pause
+
+echo ========== Cleaning up... ==========
+
+rem rmdir Common\bin\navigator\debug /S /Q
+rem rmdir Common\bin\navigator\release /S /Q
+
+rmdir Dependencies\llmozlib /S /Q
+rmdir Dependencies\lua /S /Q
+rmdir Dependencies\navi /S /Q
+rmdir Dependencies\navigatorModule /S /Q
+rmdir Dependencies\navigatorPlugin /S /Q
+rmdir Dependencies\ode /S /Q
+rmdir Dependencies\OgreOde /S /Q
+rmdir Dependencies\pthreads /S /Q
+rmdir Dependencies\tinyxml /S /Q
+rmdir Dependencies\tokamak /S /Q
+rmdir Dependencies\xmlrpc++ /S /Q
+
+rmdir DevTools\llmozlib\Debug_vc7 /S /Q
+rmdir DevTools\llmozlib\Debug_vc8 /S /Q
+rmdir DevTools\llmozlib\Release_vc7 /S /Q
+rmdir DevTools\llmozlib\Release_vc8 /S /Q
+rmdir DevTools\lua\lib /S /Q
+rmdir DevTools\navi\Navi\Objects_vc7 /S /Q
+rmdir DevTools\navi\Navi\Objects_vc8 /S /Q
+rmdir DevTools\navi\Navi\lib_vc7 /S /Q
+rmdir DevTools\navi\Navi\lib_vc8 /S /Q
+rmdir DevTools\navi\NaviDemo\Objects_vc7 /S /Q
+rmdir DevTools\navi\NaviDemo\Objects_vc8 /S /Q
+rmdir DevTools\navi\NaviDemo\Bin\debug_vc7 /S /Q
+rmdir DevTools\navi\NaviDemo\Bin\debug_vc8 /S /Q
+rmdir DevTools\navi\NaviDemo\Bin\release_vc7 /S /Q
+rmdir DevTools\navi\NaviDemo\Bin\release_vc8 /S /Q
+rmdir DevTools\OgreOde\lib\debug /S /Q
+rmdir DevTools\OgreOde\lib\release /S /Q
+rmdir DevTools\OgreOde\obj\Debug /S /Q
+rmdir DevTools\OgreOde\obj\Release /S /Q
+rmdir DevTools\OgreOde\ode\lib\debuglib /S /Q
+rmdir DevTools\OgreOde\ode\lib\releaselib /S /Q
+rmdir DevTools\OgreOde\ode\build\vs2003\obj /S /Q
+rmdir DevTools\OgreOde\ode\build\vs2005\obj /S /Q
+del DevTools\pthreads\pthreadVC* /S /Q
+rmdir DevTools\tinyxml\Debug /S /Q
+rmdir DevTools\tinyxml\Release /S /Q
+rmdir DevTools\tokamak\lib\debugdll /S /Q
+rmdir DevTools\tokamak\lib\releasedll /S /Q
+rmdir DevTools\tokamak\tokamaksrc\tokamakdll /S /Q
+del DevTools\xmlrpc++\lib\*.lib /S /Q
+rmdir DevTools\xmlrpc++\Debug /S /Q
+rmdir DevTools\xmlrpc++\Release /S /Q
+
+rmdir Docs\doxygen\html /S /Q
+
+rmdir navigator\Debug /S /Q
+rmdir navigator\Release /S /Q
+
+rmdir navigatorModule\Debug /S /Q
+rmdir navigatorModule\Release /S /Q
+
+rmdir navigatorPlugin\navigatorActiveX\Debug_vc7 /S /Q
+rmdir navigatorPlugin\navigatorActiveX\Debug_vc8 /S /Q
+rmdir navigatorPlugin\navigatorActiveX\Release_vc7 /S /Q
+rmdir navigatorPlugin\navigatorActiveX\Release_vc8 /S /Q
+rmdir navigatorPlugin\navigatorPlugin\Debug_vc7 /S /Q
+rmdir navigatorPlugin\navigatorPlugin\Debug_vc8 /S /Q
+rmdir navigatorPlugin\navigatorPlugin\Release_vc7 /S /Q
+rmdir navigatorPlugin\navigatorPlugin\Release_vc8 /S /Q
+
+rmdir navigatorRunnerPlugin\navigatorRunnerAx\Debug_vc7 /S /Q
+rmdir navigatorRunnerPlugin\navigatorRunnerAx\Debug_vc8 /S /Q
+rmdir navigatorRunnerPlugin\navigatorRunnerAx\Release_vc7 /S /Q
+rmdir navigatorRunnerPlugin\navigatorRunnerAx\Release_vc8 /S /Q
+rmdir navigatorRunnerPlugin\navigatorRunnerMoz\Debug_vc7 /S /Q
+rmdir navigatorRunnerPlugin\navigatorRunnerMoz\Debug_vc8 /S /Q
+rmdir navigatorRunnerPlugin\navigatorRunnerMoz\Release_vc7 /S /Q
+rmdir navigatorRunnerPlugin\navigatorRunnerMoz\Release_vc8 /S /Q
+
+echo ============== Done! ===============
+pause
