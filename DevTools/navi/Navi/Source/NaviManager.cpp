737b91feadfba168bfe9426358f885620b696108
Stéphane Chaplain (schaplain) <s.chaplain@artefacto.fr>
2008-01-25 08:28:12 +0000

    FIX VC7 compilation with operator compare on navis
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@426 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- DevTools/navi/Navi/Source/NaviManager.cpp
+++ DevTools/navi/Navi/Source/NaviManager.cpp
@@ -301,6 +301,13 @@ bool NaviManager::injectMouseUp(int buttonID)
 	return false;
 }
 
+struct compare { 
+	bool operator()(Navi* a, Navi* b)
+	{
+		return(*a > *b); 
+	};
+};
+
 bool NaviManager::focusNavi(int x, int y, Navi* selection)
 {
 	deFocusAllNavis();
@@ -319,7 +326,7 @@ bool NaviManager::focusNavi(int x, int y, Navi* selection)
 		if(!iter->second->isMaterial)
 			sortedNavis.push_back(iter->second);
 
-	struct compare { bool operator()(Navi* a, Navi* b){ return(a->overlay->getZOrder() > b->overlay->getZOrder()); }};
+	
 	std::sort(sortedNavis.begin(), sortedNavis.end(), compare());
 
 	if(sortedNavis.size())
