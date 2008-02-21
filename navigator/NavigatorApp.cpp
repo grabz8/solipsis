99719a0630dbeab10d2aa700e94f5cf0efdd6bde
Jangre (jangre) <>
2008-02-21 15:02:40 +0000

    setlocale() added to ensure NUMERIC representation compatibility between hosts
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@534 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- navigator/NavigatorApp.cpp
+++ navigator/NavigatorApp.cpp
@@ -21,6 +21,7 @@ INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
 int main(int argc, char *argv[])
 #endif
 {
+    setlocale(LC_NUMERIC, "English");
     try
     {
         // Create application
