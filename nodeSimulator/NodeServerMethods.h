8975644d7c87fb23190c78869c0e1c156f6fa086
Jangre (jangre) <>
2008-02-21 15:38:49 +0000

    Peer/Navigator beta-developments
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@542 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- nodeSimulator/NodeServerMethods.h
+++
@@ -1,9 +0,0 @@
-#ifndef __NodeServerMethods_h__
-#define __NodeServerMethods_h__
-
-#include "LoginMethod.h"
-#include "HandleEvtMethod.h"
-#include "SendEvtMethod.h"
-#include "LogoutMethod.h"
-
-#endif // #ifndef __NodeServerMethods_h__
