d350ff241f92d6195e5f3d5708e9f902c1ca1afa
Stéphane Chaplain (schaplain) <s.chaplain@artefacto.fr>
2008-03-18 16:28:20 +0000

    modification of the offset / speed animation for the run and walk animation
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@582 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- navigatorModule/Avatar.cpp
+++ navigatorModule/Avatar.cpp
@@ -350,6 +350,7 @@ void Avatar::animate(Real timeSinceLastFrame)
     Vector3 mvt = Vector3(0, 0, 0);
     State nextState = mState;
     Real animOffset = 0;
+    Real nbFrames = 0.;
 
     mUpKeyMotion.update(timeSinceLastFrame);
     mDownKeyMotion.update(timeSinceLastFrame);
@@ -393,14 +394,22 @@ void Avatar::animate(Real timeSinceLastFrame)
 //    if ((Math::Abs(upDownMvt) > MAX_SPEED*0.9) && (mState != SFly))
 //        nextState = SFly;
 
+    // Get number frames on Idle animation
+    //setState(SIdle);
+    //Real nbFramesIdle = mAnimationState->getLength();
+    //setState(mState);
+
+    nbFrames = mAnimationState->getLength();
     if ((mState == SWalk) || (mState == SRun))
-        if (Math::Abs(frontBackMvt) > EPSILON_SPEED)
-            animOffset = (frontBackMvt/(MAX_SPEED/5))*timeSinceLastFrame;
-        else if (Math::Abs(leftRightMvt) > EPSILON_SPEED)
-            animOffset = (leftRightMvt/(MAX_SPEED))*timeSinceLastFrame;
+        if (Math::Abs(frontBackMvt) > EPSILON_SPEED)        // run animation 
+            //animOffset = (frontBackMvt/(MAX_SPEED/5))*timeSinceLastFrame;
+            animOffset = (frontBackMvt/(MAX_SPEED)) / nbFrames*nbFrames*nbFrames * timeSinceLastFrame;
+        else if (Math::Abs(leftRightMvt) > EPSILON_SPEED)   // walk animation
+            //animOffset = (leftRightMvt/(MAX_SPEED))*timeSinceLastFrame;
+            animOffset = (leftRightMvt/(MAX_SPEED)) * nbFrames * timeSinceLastFrame;
         else
             nextState = SIdle;
-    else
+    else // mState = SIdle / SFly / SSwim
         animOffset = timeSinceLastFrame;
     if (mAnimationState != 0)
         mAnimationState->addTime(animOffset);
