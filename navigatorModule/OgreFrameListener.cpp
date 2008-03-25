c956cd0533f7f9b2cb5acf90ed158ee768f21d26
Stéphane Chaplain (schaplain) <s.chaplain@artefacto.fr>
2008-03-25 08:36:22 +0000

    fix : the key 'A' has been removed in the case of exit the application, in conflict with moving the avatar to the left !
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@623 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- navigatorModule/OgreFrameListener.cpp
+++ navigatorModule/OgreFrameListener.cpp
@@ -71,7 +71,7 @@ bool OgreFrameListener::keyPressed(const KeyboardEvt& evt)
 { 
     switch (evt.mKey)
     {
-    case KC_A: 
+    //case KC_A:        // removed to control the avatar's moves
     case KC_ESCAPE: 
         mContinue = false;
         break;
