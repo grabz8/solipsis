2f92bc563f0b62e01b8784d14545ceb1e2440d6c
Jangre (jangre) <>
2008-03-31 13:54:39 +0000

    bug into sub-dirs copy
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@672 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- navigatorModuleOnlyNav/copyNaviLocal.bat
+++ navigatorModuleOnlyNav/copyNaviLocal.bat
@@ -6,9 +6,9 @@ if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.js .
 if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.png ..\Common\bin\navigator\Debug\NaviLocal /s /y
 if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.gif ..\Common\bin\navigator\Debug\NaviLocal /s /y
 if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\*.txt ..\Common\bin\navigator\Debug\NaviLocal /s /y
-if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\color ..\Common\bin\navigator\Debug\NaviLocal /s /y
-if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\slider ..\Common\bin\navigator\Debug\NaviLocal /s /y
-if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\tabber ..\Common\bin\navigator\Debug\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\color ..\Common\bin\navigator\Debug\NaviLocal\color /s /y
+if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\slider ..\Common\bin\navigator\Debug\NaviLocal\slider /s /y
+if exist ..\Common\bin\navigator\Debug\NaviLocal xcopy ..\Media\NaviLocal\tabber ..\Common\bin\navigator\Debug\NaviLocal\tabber /s /y
 
 if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.html ..\Common\bin\navigator\Release\NaviLocal /s /y
 if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.css ..\Common\bin\navigator\Release\NaviLocal /s /y
@@ -16,8 +16,8 @@ if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.js
 if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.png ..\Common\bin\navigator\Release\NaviLocal /s /y
 if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.gif ..\Common\bin\navigator\Release\NaviLocal /s /y
 if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\*.txt ..\Common\bin\navigator\Release\NaviLocal /s /y
-if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\color ..\Common\bin\navigator\Release\NaviLocal /s /y
-if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\slider ..\Common\bin\navigator\Release\NaviLocal /s /y
-if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\tabber ..\Common\bin\navigator\Release\NaviLocal /s /y
+if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\color ..\Common\bin\navigator\Release\NaviLocal\color /s /y
+if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\slider ..\Common\bin\navigator\Release\NaviLocal\slider /s /y
+if exist ..\Common\bin\navigator\Release\NaviLocal xcopy ..\Media\NaviLocal\tabber ..\Common\bin\navigator\Release\NaviLocal\tabber /s /y
 
 pause
