ac281970f42db17516842cb2db28f6fd00489001
Jangre (jangre) <>
2008-04-01 12:58:53 +0000

    doxygen updated with peer/navigator architecture
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@684 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Docs/doxygen/cleanup.bat
+++ Docs/doxygen/cleanup.bat
@@ -2,6 +2,8 @@
 
 echo ========== Cleaning up... ==========
 
+del *.log
+
 del navigatorModule\html /S /Q
 rmdir navigatorModule /S /Q
 
@@ -11,6 +13,18 @@ del navigatorPhysicsPhysX\html /S /Q
 rmdir navigatorPhysicsPhysX /S /Q
 del navigatorPhysicsTokamak\html /S /Q
 rmdir navigatorPhysicsTokamak /S /Q
+
+del navigatorModule\html /S /Q
+rmdir navigatorModule /S /Q
+
+del peerModuleSimulator\html /S /Q
+rmdir peerModuleSimulator /S /Q
+
+del peerPhysicsPhysX\html /S /Q
+rmdir peerPhysicsPhysX /S /Q
+
+del protocols\html /S /Q
+rmdir protocols /S /Q
 
 echo ============== Done! ===============
 pause
--- Docs/doxygen/doxygen.bat
+++ Docs/doxygen/doxygen.bat
@@ -1,8 +1,8 @@
 @echo off
 
-%DOXYGEN_PATH%\doxygen.exe Doxyfile4navigatorModule > Doxyfile4navigatorModule.log 2>&1
-%DOXYGEN_PATH%\doxygen.exe Doxyfile4navigatorPlugin_ODE > Doxyfile4navigatorPlugin_ODE.log 2>&1
-%DOXYGEN_PATH%\doxygen.exe Doxyfile4navigatorPlugin_PhysX > Doxyfile4navigatorPlugin_PhysX.log 2>&1
-%DOXYGEN_PATH%\doxygen.exe Doxyfile4navigatorPlugin_Tokamak > Doxyfile4navigatorPlugin_Tokamak.log 2>&1
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4navigatorModuleOnlyNav > Doxyfile4navigatorModuleOnlyNav.log 2>&1
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4peerModuleSimulator > Doxyfile4peerModuleSimulator.log 2>&1
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4peerPlugin_PhysX > Doxyfile4peerPlugin_PhysX.log 2>&1
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4protocols > Doxyfile4protocols.log 2>&1
 
 pause
