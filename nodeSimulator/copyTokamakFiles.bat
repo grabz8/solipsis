9308bcbc58db011cd9d7355157f08c4296f574f8
Jangre (jangre) <>
2008-04-02 15:08:42 +0000

    nodeSimulator replaced by peerModuleSimulator
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@688 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- nodeSimulator/copyTokamakFiles.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-if exist ..\Common\bin\nodeSimulator\Debug copy ..\Dependencies\tokamak\lib\debugdll\tokamakdll.dll ..\Common\bin\nodeSimulator\Debug /y
-
-if exist ..\Common\bin\nodeSimulator\Release copy ..\Dependencies\tokamak\lib\releasedll\tokamakdll.dll ..\Common\bin\nodeSimulator\Release /y
-
-pause
