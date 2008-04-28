5506b5b28bb1535340f8e797f44de7bd12aa4ab6
Jangre (jangre) <>
2008-04-28 15:14:14 +0000

    CommonTools library added to migrate all crossplatforms helpers classes into same lib (System, BasicThread, ...)
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@749 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- peerModuleSimulator/PlatformWin.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PlatformWin_h__
-#define __PlatformWin_h__
-
-#ifndef WIN32_LEAN_AND_MEAN
-#  define WIN32_LEAN_AND_MEAN
-#endif
-#include "windows.h"
-
-#endif // #ifndef __PlatformWin_h__
