2aaac4848c90b4de9056b88aa6b66b132fb2412f
Jangre (jangre) <>
2008-11-12 10:19:07 +0000

    boot.lua updated to use our public WorldsServer + localWorldAddress/login now empty
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1051 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Media/lua/boot.lua
+++ Media/lua/boot.lua
@@ -37,10 +37,10 @@ end
 navigator = NavigatorLua()
 navigator:bind()
 navigator:setNameValueVariable("PeerAddress", "localhost:8880")
-navigator:setNameValueVariable("LocalWorldAddress", "localhost:8660")
-navigator:setNameValueVariable("WorldServerAddress", "localhost:8550")
+navigator:setNameValueVariable("LocalWorldAddress", "")
+navigator:setNameValueVariable("WorldServerAddress", "80.13.207.29:8550")
 navigator:setNameValueVariable("WorldServerTimeout", "8")
-navigator:setNameValueVariable("Login", "me")
+navigator:setNameValueVariable("Login", "")
 navigator:setNameValueVariable("FacebookApiKey", "8d81e4c64ac0039b209c4a53b21ba220");
 navigator:setNameValueVariable("FacebookServer", "api.facebook.com/restserver.php");
 navigator:setNameValueVariable("FacebookLoginUrl", "http://api.facebook.com/login.php");
