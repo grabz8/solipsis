d0cdbd6ee32cd8047b1e29753bee3872b2a9cca1
Jangre (jangre) <>
2008-11-24 16:16:58 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1087 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- CommonTools/CTIO.cpp
+++ CommonTools/CTIO.cpp
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "CTIO.h"
 
-#if defined(_WINDOWS)
+#ifdef WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <sys\stat.h>
