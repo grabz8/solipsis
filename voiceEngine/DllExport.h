0886ad0b91f11aae64f93de45a7426959d4aa51c
raffygu <>
2008-11-27 16:33:54 +0000

    oubli
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1124 53240bf5-9d7a-4d4b-845a-3b2958f72d47

---
+++ voiceEngine/DllExport.h
@@ -0,0 +1,33 @@
+/*
+This source file is part of Solipsis
+    (Solipsis is an opensource decentralized Metaverse platform)
+For the latest info, see http://www.solipsis.org/
+
+Copyright (C) 2006-2008 ANR-RIAM (IRISA, Archivideo, Artefacto, Rennes 2 University, Orange Labs)
+Author Guillaume Raffy (Proservia)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef __DllExport_h__
+#define __DllExport_h__
+
+#ifdef BUILDING_VOICEENGINE
+	#define VOICEENGINE_EXPORT __declspec( dllexport )
+#else
+	#define VOICEENGINE_EXPORT
+#endif 
+
+#endif // #ifndef __DllExport_h__
