04c68fe2f1f1f867e83fc0bf8db935650ddd85a5
Jangre (jangre) <>
2008-11-27 13:36:13 +0000

    Mouse cursor is now exclusive only on 1st person camera mode, mouse can leave window
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1118 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- navigatorPlugin/navigatorActiveX/navigatorActiveXCtrl.h
+++ navigatorPlugin/navigatorActiveX/navigatorActiveXCtrl.h
@@ -78,6 +78,7 @@ public:
 
     // overridden from NavigatorModule::IWindow
     virtual void * getHandle() { return mhWnd; }
+    virtual bool isFullscreen() { return false; }
     virtual unsigned int getWidth() { return mWidth; }
     virtual unsigned int getHeight() { return mHeight; }
     virtual void setMouseExclusive(bool exclusive) {}
