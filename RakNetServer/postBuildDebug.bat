9ffd483d3e735147d9a68d62cfb195a0e94009c8
Jangre (jangre) <>
2008-12-05 16:54:17 +0000

    libiconv added into DevTools to convert WCHAR_T (ie std::wstring) in UTF8 chat panel is now internationalized  - 1 TrueTypeFont to display special wide-chars,  - conversions: HTML chat panel (URI-encoded) -> defaultGUI.lua -> XmlAction::desc (wstring) -> XML (UTF8 + xmlEscaped) -> RakNet String serialization
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1140 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- RakNetServer/postBuildDebug.bat
+++ RakNetServer/postBuildDebug.bat
@@ -8,5 +8,6 @@ if exist "%dst%\OgreMain_d.dll" goto OgreMain_d_exist
 if not exist "%dst%\pthreadVC_d.dll" xcopy "..\Dependencies\pthreads\lib\pthreadVC_d.dll" "%dst%" /s /y
 xcopy "..\Dependencies\protocols\XmlDatas\lib\DebugDLL\XmlDatas.dll" "%dst%" /s /y
 if not exist "%dst%\RakNetDebug.dll" xcopy "..\Dependencies\RakNet\lib\RakNetDebug.dll" "%dst%" /s /y
+xcopy "..\Dependencies\libiconv\lib\libiconv2.dll" "%dst%" /s /y
 
 copy "readme.txt" "%dst%" /y
