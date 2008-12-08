107019ab29795faf077ebcfce7e12720f18ac768
Jangre (jangre) <>
2008-12-08 12:25:49 +0000

    Ogre 160RC1 std::min/max fix
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1141 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- voiceEngine/voiceEngineFModSpeex/FModSpeexPluginDll.cpp
+++ voiceEngine/voiceEngineFModSpeex/FModSpeexPluginDll.cpp
@@ -21,9 +21,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#include <Ogre.h>
 #include "FModSpeexPlugin.h"
 #include "FModSpeexPluginPrerequisites.h"
-#include "OgreRoot.h"
 
 using namespace Ogre;
 
