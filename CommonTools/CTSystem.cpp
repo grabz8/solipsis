d4277a7fcac69feb902f1bd24f68950203407521
Jangre (jangre) <>
2009-01-30 10:11:23 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1260 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- CommonTools/CTSystem.cpp
+++ CommonTools/CTSystem.cpp
@@ -34,7 +34,7 @@ std::string System::getDateTimeYYYYMMDDHHMMSS()
     time(&now);
     struct tm *now_tm = localtime(&now);
     char timeBuf[16];
-    _snprintf(timeBuf, sizeof(timeBuf) - 1, "%d%d%d%d%d%d", now_tm->tm_year + 1900, now_tm->tm_mon + 1, now_tm->tm_mday, now_tm->tm_hour, now_tm->tm_min, now_tm->tm_sec);
+    _snprintf(timeBuf, sizeof(timeBuf) - 1, "%d%02d%02d%02d%02d%02d", now_tm->tm_year + 1900, now_tm->tm_mon + 1, now_tm->tm_mday, now_tm->tm_hour, now_tm->tm_min, now_tm->tm_sec);
     timeBuf[sizeof(timeBuf) - 1] = '\0';
     return timeBuf;
 }
