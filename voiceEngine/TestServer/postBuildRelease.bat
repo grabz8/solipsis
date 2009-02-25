9a80f697d8b5d55d5ac6294106ac80e39922a121
Christophe Floutier (cfloutier) <cfloutier@gmail.com>
2009-02-25 16:35:16 +0000

    réarchitecture : réunion des tout ce qui est en rapport avec la VOIP dans Voice (phonetizer et plugins comppris)
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1362 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- voiceEngine/TestServer/postBuildRelease.bat
+++
@@ -1,6 +0,0 @@
-set dst="..\..\Common\bin\VoiceEngineServer\Release"
-
-if not exist "%dst%" mkdir "%dst%"
-if not exist "%dst%\pthreadVC.dll" xcopy "..\..\Dependencies\pthreads\lib\pthreadVC.dll" "%dst%" /s /y
-
-copy "readme.txt" "%dst%" /y
