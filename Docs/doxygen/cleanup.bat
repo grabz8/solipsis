bd8ce4f85ff12f73d47475e20349bdb525d5c1ed
Christophe Floutier (cfloutier) <cfloutier@gmail.com>
2009-02-26 15:14:23 +0000

    réarchitecture : mise à jour de dowygen
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1369 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Docs/doxygen/cleanup.bat
+++ Docs/doxygen/cleanup.bat
@@ -4,27 +4,29 @@ echo ========== Cleaning up... ==========
 
 del *.log
 
+del CommonTools\html /S /Q
+rmdir CommonTools /S /Q
+
+del Communication\html /S /Q
+rmdir Communication /S /Q
+
+del Modelers\html /S /Q
+rmdir Modelers /S /Q
+
+del NavigatorModules\html /S /Q
+rmdir NavigatorModules /S /Q
+
 del navigatorModule\html /S /Q
 rmdir navigatorModule /S /Q
 
-del navigatorPhysicsODE\html /S /Q
-rmdir navigatorPhysicsODE /S /Q
-del navigatorPhysicsPhysX\html /S /Q
-rmdir navigatorPhysicsPhysX /S /Q
-del navigatorPhysicsTokamak\html /S /Q
-rmdir navigatorPhysicsTokamak /S /Q
+del PeerModules\html /S /Q
+rmdir PeerModules /S /Q
 
-del navigatorModule\html /S /Q
-rmdir navigatorModule /S /Q
+del RackNetServer\html /S /Q
+rmdir RackNetServer /S /Q
 
-del peerModuleSimulator\html /S /Q
-rmdir peerModuleSimulator /S /Q
-
-del peerPhysicsPhysX\html /S /Q
-rmdir peerPhysicsPhysX /S /Q
-
-del protocols\html /S /Q
-rmdir protocols /S /Q
+del Voice\html /S /Q
+rmdir Voice /S /Q
 
 echo ============== Done! ===============
 pause
--- Docs/doxygen/doxygen.bat
+++ Docs/doxygen/doxygen.bat
@@ -1,8 +1,18 @@
 @echo off
 
-"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4navigatorModuleOnlyNav > Doxyfile4navigatorModuleOnlyNav.log 2>&1
-"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4peerModuleSimulator > Doxyfile4peerModuleSimulator.log 2>&1
-"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4peerPlugin_PhysX > Doxyfile4peerPlugin_PhysX.log 2>&1
-"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4protocols > Doxyfile4protocols.log 2>&1
+echo CommonTools
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4CommonTools.dox > Doxyfile4CommonTools.log 2>&1
+echo Communication
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4Communication.dox > Doxyfile4Communication.log 2>&1
+echo Navigator
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4Navigator.dox > Doxyfile4Navigator.log 2>&1
+echo Peer
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4Peer.dox > Doxyfile4Peer.log 2>&1
+echo Voice
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4Voice.dox > Doxyfile4Voice.log 2>&1
+echo Modelers
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4Modelers.dox > Doxyfile4Modelers.log 2>&1
+echo RakNetServer
+"%DOXYGEN_PATH%\doxygen.exe" Doxyfile4RakNetServer.dox > Doxyfile4RakNetServer.log 2>&1
 
 pause
