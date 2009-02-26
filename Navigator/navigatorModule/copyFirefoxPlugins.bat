154f3327980869c12c82dc3cae6d0237a4681740
Jangre (jangre) <>
2009-02-26 10:49:55 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1364 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Navigator/navigatorModule/copyFirefoxPlugins.bat
+++ Navigator/navigatorModule/copyFirefoxPlugins.bat
@@ -7,13 +7,13 @@ echo Plugin file NPSWF32.dll not found ! Check your Adobe Flash Player installat
 goto End
 
 :InMozilla_exist
-if exist ..\Common\bin\navigator\Debug\plugins copy "C:\Program Files\Mozilla Firefox\plugins\NPSWF32.dll" ..\Common\bin\navigator\Debug\plugins /y
-if exist ..\Common\bin\navigator\Release\plugins copy "C:\Program Files\Mozilla Firefox\plugins\NPSWF32.dll" ..\Common\bin\navigator\Release\plugins /y
+if exist ..\..\Common\bin\navigator\Debug\plugins copy "C:\Program Files\Mozilla Firefox\plugins\NPSWF32.dll" ..\..\Common\bin\navigator\Debug\plugins /y
+if exist ..\..\Common\bin\navigator\Release\plugins copy "C:\Program Files\Mozilla Firefox\plugins\NPSWF32.dll" ..\..\Common\bin\navigator\Release\plugins /y
 goto End
 
 :InWinSys32MacromedFlash_exist
-if exist ..\Common\bin\navigator\Debug\plugins copy "C:\WINDOWS\system32\Macromed\Flash\NPSWF32.dll" ..\Common\bin\navigator\Debug\plugins /y
-if exist ..\Common\bin\navigator\Release\plugins copy "C:\WINDOWS\system32\Macromed\Flash\NPSWF32.dll" ..\Common\bin\navigator\Release\plugins /y
+if exist ..\..\Common\bin\navigator\Debug\plugins copy "C:\WINDOWS\system32\Macromed\Flash\NPSWF32.dll" ..\..\Common\bin\navigator\Debug\plugins /y
+if exist ..\..\Common\bin\navigator\Release\plugins copy "C:\WINDOWS\system32\Macromed\Flash\NPSWF32.dll" ..\..\Common\bin\navigator\Release\plugins /y
 goto End
 
 :End
