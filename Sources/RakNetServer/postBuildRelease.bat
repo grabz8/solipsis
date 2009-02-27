daeaf33ac0dcacfc8fcd06abfa613688ca408b19
Jangre (jangre) <>
2009-02-27 09:31:48 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1377 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Sources/RakNetServer/postBuildRelease.bat
+++ Sources/RakNetServer/postBuildRelease.bat
@@ -5,9 +5,9 @@ if exist "%dst%\OgreMain.dll" goto OgreMain_exist
  if exist "%OGRE_HOME%\lib\OgreMain.dll" xcopy "%OGRE_HOME%\lib\OgreMain.dll" "%dst%" /s /y
  if exist "%OGRE_HOME%\bin\release\OgreMain.dll" xcopy "%OGRE_HOME%\bin\release\OgreMain.dll" "%dst%" /s /y
 :OgreMain_exist
-if not exist "%dst%\pthreadVC.dll" xcopy "..\..\..\Dependencies\pthreads\lib\pthreadVC.dll" "%dst%" /s /y
-xcopy "..\..\..\Dependencies\protocols\XmlDatas\lib\ReleaseDLL\XmlDatas.dll" "%dst%" /s /y
-if not exist "%dst%\RakNet.dll" xcopy "..\..\..\Dependencies\RakNet\lib\RakNet.dll" "%dst%" /s /y
-xcopy "..\..\..\Dependencies\libiconv\lib\libiconv2.dll" "%dst%" /s /y
+if not exist "%dst%\pthreadVC.dll" xcopy "..\..\Dependencies\pthreads\lib\pthreadVC.dll" "%dst%" /s /y
+xcopy "..\..\Dependencies\protocols\XmlDatas\lib\ReleaseDLL\XmlDatas.dll" "%dst%" /s /y
+if not exist "%dst%\RakNet.dll" xcopy "..\..\Dependencies\RakNet\lib\RakNet.dll" "%dst%" /s /y
+xcopy "..\..\Dependencies\libiconv\lib\libiconv2.dll" "%dst%" /s /y
 
 copy "readme.txt" "%dst%" /y
