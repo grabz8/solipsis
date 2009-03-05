f57fb3ecfbc7ee33770f8716488ac3a23b19ffa2
Christophe Floutier (cfloutier) <cfloutier@gmail.com>
2009-03-05 13:24:33 +0000

    ajout des scripts pour vc9
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1393 53240bf5-9d7a-4d4b-845a-3b2958f72d47

---
+++ Sources/BatchBuild/vc9_CleanUpAndBuild.bat
@@ -0,0 +1,15 @@
+cd ..
+set PATH=C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE;C:\Program Files\Subversion\bin;%PATH%
+
+pushd ..
+call cleanup.bat
+popd
+
+devenv "3rdParties_vc9.sln" /build "Release|Win32" /out buildLog.log
+devenv "3rdParties_vc9.sln" /build "Debug|Win32" /out buildLog.log
+
+devenv "solipsis_vc9.sln" /build "Release|Win32" /out buildLog.log 
+devenv "solipsis_vc9.sln" /build "Debug|Win32" /out buildLog.log
+
+pause
+
