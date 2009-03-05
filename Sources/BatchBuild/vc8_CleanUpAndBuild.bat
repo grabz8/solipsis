2f246ee7f1cf714e3b1a551f5f964a1b99c5c75a
Christophe Floutier (cfloutier) <cfloutier@gmail.com>
2009-03-05 13:17:26 +0000

    déplacement des batch de compil et rennomage
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1392 53240bf5-9d7a-4d4b-845a-3b2958f72d47

---
+++ Sources/BatchBuild/vc8_CleanUpAndBuild.bat
@@ -0,0 +1,15 @@
+cd ..
+set PATH=C:\Program Files\Microsoft Visual Studio 8\Common7\IDE;C:\Program Files\Subversion\bin;%PATH%
+
+pushd ..
+call cleanup.bat
+popd
+
+devenv "3rdParties_vc8.sln" /build "Release|Win32" /out buildLog.log
+devenv "3rdParties_vc8.sln" /build "Debug|Win32" /out buildLog.log
+
+devenv "solipsis_vc8.sln" /build "Release|Win32" /out buildLog.log 
+devenv "solipsis_vc8.sln" /build "Debug|Win32" /out buildLog.log
+
+pause
+
