962118c1b14769673ebf07ff05e91f3554258ee5
Jangre (jangre) <>
2009-03-10 13:13:31 +0000

    RakNet connections lost: timeout param increased to 15s + re-connection RakNet connections lost: error dlg msg added + disconnect/re-connect RakNet connections lost: ghost avatar/character added + Kevin.saf alpha_rejection param removed Cache Manager: now files transfers are performed by chunks of 4Kb to avoid bandwidth saturation CommonTools: added path separator accessor
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1410 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Sources/DevTools/RakNet/Source/FormatString.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FORMAT_STRING_H
-#define __FORMAT_STRING_H
-
-char * FormatString(const char *format, ...);
-
-
-#endif
-
--- Sources/DevTools/RakNet/Source/RakNetVersion.h
+++
@@ -1,5 +0,0 @@
-#define RAKNET_VERSION "3.24"
-
-#define RAKNET_REVISION "$Revision$"
-
-#define RAKNET_DATE "$Date$"
