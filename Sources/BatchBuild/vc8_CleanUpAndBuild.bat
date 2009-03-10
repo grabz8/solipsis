161ebb4ba748306fe463cb60567112323f67efb3
Christophe Floutier (cfloutier) <cfloutier@gmail.com>
2009-03-10 14:29:33 +0000

    adaptation batch build à visual express
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1415 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Sources/BatchBuild/vc8_CleanUpAndBuild.bat
+++ Sources/BatchBuild/vc8_CleanUpAndBuild.bat
@@ -1,15 +1,18 @@
 cd ..
 set PATH=C:\Program Files\Microsoft Visual Studio 8\Common7\IDE;C:\Program Files\Subversion\bin;%PATH%
 
+set DEVENV=devenv
+if not exist "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" set DEVENV=VCExpress
+
 pushd ..
 call cleanup.bat
 popd
 
-devenv "3rdParties_vc8.sln" /build "Release|Win32" /out buildLog.log
-devenv "3rdParties_vc8.sln" /build "Debug|Win32" /out buildLog.log
+%DEVENV% "3rdParties_vc8.sln" /build "Release|Win32" /out buildLog.log
+%DEVENV% "3rdParties_vc8.sln" /build "Debug|Win32" /out buildLog.log
 
-devenv "solipsis_vc8.sln" /build "Release|Win32" /out buildLog.log 
-devenv "solipsis_vc8.sln" /build "Debug|Win32" /out buildLog.log
+%DEVENV% "solipsis_vc8.sln" /build "Release|Win32" /out buildLog.log 
+%DEVENV% "solipsis_vc8.sln" /build "Debug|Win32" /out buildLog.log
 
 pause
 
--- Sources/BatchBuild/vc9_CleanUpAndBuildNCS.bat
+++ Sources/BatchBuild/vc9_CleanUpAndBuildNCS.bat
@@ -1,15 +1,18 @@
 cd ..
 set PATH=C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE;C:\Program Files\Subversion\bin;%PATH%
 
+set DEVENV=devenv
+if not exist "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" set DEVENV=VCExpress
+
 pushd ..
 call cleanup.bat
 popd
 
-devenv "3rdParties_vc9.sln" /build "Release|Win32" /out buildLog.log
-devenv "3rdParties_vc9.sln" /build "Debug|Win32" /out buildLog.log
+%DEVENV% "3rdParties_vc9.sln" /build "Release|Win32" /out buildLog.log
+%DEVENV% "3rdParties_vc9.sln" /build "Debug|Win32" /out buildLog.log
 
-devenv "solipsis_vc9.sln" /build "ReleaseNCS|Win32" /out buildLog.log 
-devenv "solipsis_vc9.sln" /build "DebugNCS|Win32" /out buildLog.log
+%DEVENV% "solipsis_vc9.sln" /build "ReleaseNCS|Win32" /out buildLog.log 
+%DEVENV% "solipsis_vc9.sln" /build "DebugNCS|Win32" /out buildLog.log
 
 pause
 
