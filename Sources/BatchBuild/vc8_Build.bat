4a5ddb8efe56e75c0a3d5bae8955bfb2c1feaf4e
Christophe Floutier (cfloutier) <cfloutier@gmail.com>
2009-03-12 11:40:45 +0000

    clean up log file for each build
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1421 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Sources/BatchBuild/vc8_Build.bat
+++ Sources/BatchBuild/vc8_Build.bat
@@ -1,6 +1,8 @@
 cd ..
 set PATH=C:\Program Files\Microsoft Visual Studio 8\Common7\IDE;C:\Program Files\Subversion\bin;%PATH%
 
+del buildLog.log
+
 set DEVENV=devenv
 if not exist "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" set DEVENV=VCExpress
 
