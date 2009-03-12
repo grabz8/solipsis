f446e17d78268d756597fb4db384d33ad52a6ed9
Christophe Floutier (cfloutier) <cfloutier@gmail.com>
2009-03-12 11:41:07 +0000

    adding progress bar object
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1422 53240bf5-9d7a-4d4b-845a-3b2958f72d47

---
+++ Sources/Navigator/navigatorModule/OgreGraphicObjects/ProgressBar.cpp
@@ -0,0 +1,428 @@
+/*
+This source file is part of Solipsis
+    (Solipsis is an opensource decentralized Metaverse platform)
+For the latest info, see http://www.solipsis.org/
+
+Copyright (C) 2006-2008 ANR-RIAM (IRISA, Archivideo, Artefacto, Rennes 2 University, Orange Labs)
+Author Christophe Floutier
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/  
+
+#include "Prerequisites.h"
+#include "ProgressBar.h"
+
+#define POS_TEX_BINDING    0
+#define COLOUR_BINDING     1
+#if OGRE_VERSION_MAJOR >= 1
+#if OGRE_VERSION_MINOR >= 4
+#define OGRE_VERS_GREATER_EIHORT
+#endif
+#endif
+
+using namespace Solipsis;
+
+////////////////////// the Simple Progress Bar //////////////////////////////////////
+
+ProgressBar::ProgressBar(const Ogre::String & name, Real width, Real height, bool applyParentScale) 
+{
+	mRenderOp.vertexData = NULL;
+	mApplyParentScale = applyParentScale;
+	mwidth = width;
+	mheight = height;
+	mOnTop = false;
+	mProgress = 0;
+
+	mMaterialName = "Examples/ProgressBar";
+
+	mNeedUpdateMaterial = true;
+	mNeedUpdateGeometry = true;
+
+	mwidth = width;
+
+	_setupGeometry();
+}
+void ProgressBar::setProgress(Real value)
+{
+	if (value < 0)
+		value = 0;
+	else if (value > 1)
+		value = 1;
+
+	mProgress = value;
+	mNeedUpdateGeometry = true;
+}
+
+ProgressBar::~ProgressBar()
+{
+	if (mRenderOp.vertexData)
+		delete mRenderOp.vertexData;
+}
+
+/*
+
+ManualObject* myManualObject =  mSceneMgr->createManualObject("manual1"); 
+SceneNode* myManualObjectNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("manual1_node"); 
+
+MaterialPtr myManualObjectMaterial = MaterialManager::getSingleton().create("manual1Material","debugger"); 
+myManualObjectMaterial->setReceiveShadows(false); 
+myManualObjectMaterial->getTechnique(0)->setLightingEnabled(true); 
+myManualObjectMaterial->getTechnique(0)->getPass(0)->setDiffuse(0,0,1,0); 
+myManualObjectMaterial->getTechnique(0)->getPass(0)->setAmbient(0,0,1); 
+myManualObjectMaterial->getTechnique(0)->getPass(0)->setSelfIllumination(0,0,1); 
+
+myManualObject->begin("manual1Material", Ogre::RenderOperation::OT_LINE_LIST); 
+myManualObject->position(3, 2, 1); 
+myManualObject->position(4, 1, 0); 
+// etc 
+myManualObject->end(); 
+
+myManualObjectNode->attachObject(myManualObject);
+
+*/
+void ProgressBar::_setupMaterial()
+{
+	if (!mpMaterial.isNull())
+	{
+		mpMaterial->unload();
+//		MaterialManager::getSingleton().unload(mpMaterial);
+	}
+	
+	mpMaterial = MaterialManager::getSingleton().load(mMaterialName, "Examples");
+	mNeedUpdateMaterial = false;
+}
+
+void ProgressBar::_setupGeometry()
+{
+	if (mNeedUpdateMaterial)
+		_setupMaterial();
+
+	assert(!mpMaterial.isNull());
+
+	unsigned int vertexCount = 12;
+
+	if (mRenderOp.vertexData)
+	{
+		if (mRenderOp.vertexData->vertexCount != vertexCount)
+		{
+			delete mRenderOp.vertexData;
+			mRenderOp.vertexData = NULL;
+		}
+	}
+
+	if (!mRenderOp.vertexData)
+		mRenderOp.vertexData = new Ogre::VertexData();
+
+	mRenderOp.indexData = 0;
+	mRenderOp.vertexData->vertexStart = 0;
+	mRenderOp.vertexData->vertexCount = vertexCount;
+	mRenderOp.operationType = Ogre::RenderOperation::OT_TRIANGLE_LIST; 
+	mRenderOp.useIndexes = false; 
+
+	Ogre::VertexDeclaration * decl = mRenderOp.vertexData->vertexDeclaration;
+	Ogre::VertexBufferBinding * bind = mRenderOp.vertexData->vertexBufferBinding;
+	size_t offset = 0;
+
+	// create/bind positions/tex.ccord. buffer
+	if (!decl->findElementBySemantic(Ogre::VES_POSITION))
+		decl->addElement(POS_TEX_BINDING, offset, Ogre::VET_FLOAT3, Ogre::VES_POSITION);
+
+	offset += Ogre::VertexElement::getTypeSize(Ogre::VET_FLOAT3);
+
+	if (!decl->findElementBySemantic(Ogre::VES_TEXTURE_COORDINATES))
+		decl->addElement(POS_TEX_BINDING, offset, Ogre::VET_FLOAT2, Ogre::VES_TEXTURE_COORDINATES, 0);
+
+	Ogre::HardwareVertexBufferSharedPtr ptbuf = Ogre::HardwareBufferManager::getSingleton().createVertexBuffer(decl->getVertexSize(POS_TEX_BINDING),
+		mRenderOp.vertexData->vertexCount,
+		Ogre::HardwareBuffer::HBU_DYNAMIC_WRITE_ONLY);
+	bind->setBinding(POS_TEX_BINDING, ptbuf);
+
+	// Colours - store these in a separate buffer because they change less often
+	if (!decl->findElementBySemantic(Ogre::VES_DIFFUSE))
+		decl->addElement(COLOUR_BINDING, 0, Ogre::VET_COLOUR, Ogre::VES_DIFFUSE);
+
+	Ogre::HardwareVertexBufferSharedPtr cbuf = Ogre::HardwareBufferManager::getSingleton().createVertexBuffer(decl->getVertexSize(COLOUR_BINDING),
+		mRenderOp.vertexData->vertexCount,
+		Ogre::HardwareBuffer::HBU_DYNAMIC_WRITE_ONLY);
+	bind->setBinding(COLOUR_BINDING, cbuf);
+
+	float *pVert = static_cast<float*>(ptbuf->lock(Ogre::HardwareBuffer::HBL_DISCARD));
+
+	float left = -mwidth/2;
+	float right = mwidth/2;
+
+	float top = mheight/2;
+	float bottom = -mheight/2;
+	float beginTexture = 0.55;
+	float endTexture = 1;
+
+	// each vert is (x, y, z, u, v)
+	//-------------------------------------------------------------------------------------
+	// First tri
+	//
+	// Upper left
+	*pVert++ = left;
+	*pVert++ = top;
+	*pVert++ = -1.0;
+	*pVert++ = 0;
+	*pVert++ = beginTexture;
+
+	// Bottom left
+	*pVert++ = left;
+	*pVert++ = bottom;
+	*pVert++ = -1.0;
+	*pVert++ = 0;
+	*pVert++ = endTexture;
+
+	// Top right
+	*pVert++ = right;
+	*pVert++ = top;
+	*pVert++ = -1.0;
+	*pVert++ = 1;
+	*pVert++ = beginTexture;
+	//-------------------------------------------------------------------------------------
+
+	//-------------------------------------------------------------------------------------
+	// Second tri
+	//
+	// Top right (again)
+	*pVert++ = right;
+	*pVert++ = top;
+	*pVert++ = -1.0;
+	*pVert++ = 1;
+	*pVert++ = beginTexture;
+
+	// Bottom left (again)
+	*pVert++ = left;
+	*pVert++ = bottom;
+	*pVert++ = -1.0;
+	*pVert++ = 0;
+	*pVert++ = endTexture;
+
+	// Bottom right
+	*pVert++ = right;
+	*pVert++ = bottom;
+	*pVert++ = -1.0;
+	*pVert++ = 1;
+	*pVert++ = endTexture;
+	//-------------------------------------------------------------------------------------
+
+	left = -mwidth/2;
+	right = left + mwidth*mProgress;
+
+	beginTexture = 0;
+	endTexture = 0.5;
+
+	// each vert is (x, y, z, u, v)
+	//-------------------------------------------------------------------------------------
+	// First tri
+	//
+	// Upper left
+	*pVert++ = left;
+	*pVert++ = top;
+	*pVert++ = -1;
+	*pVert++ = 0;
+	*pVert++ = beginTexture;
+
+	// Bottom left
+	*pVert++ = left;
+	*pVert++ = bottom;
+	*pVert++ = -1;
+	*pVert++ = 0;
+	*pVert++ = endTexture;
+
+	// Top right
+	*pVert++ = right;
+	*pVert++ = top;
+	*pVert++ = -1;
+	*pVert++ = 1;
+	*pVert++ = beginTexture;
+	//-------------------------------------------------------------------------------------
+
+	//-------------------------------------------------------------------------------------
+	// Second tri
+	//
+	// Top right (again)
+	*pVert++ = right;
+	*pVert++ = top;
+	*pVert++ = -1;
+	*pVert++ = 1;
+	*pVert++ = beginTexture;
+
+	// Bottom left (again)
+	*pVert++ = left;
+	*pVert++ = bottom;
+	*pVert++ = -1;
+	*pVert++ = 0;
+	*pVert++ = endTexture;
+
+	// Bottom right
+	*pVert++ = right;
+	*pVert++ = bottom;
+	*pVert++ = -1;
+	*pVert++ = 1;
+	*pVert++ = endTexture;
+	//-------------------------------------------------------------------------------------
+
+	// Unlock vertex buffer
+	ptbuf->unlock();
+
+	mAABB = Ogre::AxisAlignedBox();
+	mAABB.setMaximum(mwidth/2,mheight/2, mwidth/2);
+	mAABB.setMinimum(-mwidth/2,-mheight/2, -mwidth/2);
+
+	mRadius = Ogre::Math::Sqrt((mwidth*mwidth+mheight*mheight)/4);
+
+	mNeedUpdateGeometry = false;
+}
+
+void ProgressBar::showOnTop(bool show)
+{
+	if( mOnTop != show && !mpMaterial.isNull() )
+	{
+		mOnTop = show;
+		mpMaterial->setDepthBias(!mOnTop, 0);
+		mpMaterial->setDepthCheckEnabled(!mOnTop);
+		mpMaterial->setDepthWriteEnabled(mOnTop);
+	}
+}
+
+
+const Ogre::Quaternion & ProgressBar::getWorldOrientation(void) const
+{
+	assert(mpCam);
+	return const_cast<Ogre::Quaternion&>(mpCam->getDerivedOrientation());
+}
+
+const Ogre::Vector3 & ProgressBar::getWorldPosition(void) const
+{
+	assert(mParentNode);
+	return mParentNode->_getDerivedPosition();
+}
+
+void ProgressBar::getWorldTransforms(Ogre::Matrix4 * xform) const 
+{
+	if (this->isVisible() && mpCam)
+	{
+		Ogre::Matrix3 rot3x3, scale3x3 = Ogre::Matrix3::IDENTITY;
+
+		// store rotation in a matrix
+		mpCam->getDerivedOrientation().ToRotationMatrix(rot3x3);
+
+		// parent node position
+		Ogre::Vector3 ppos = mParentNode->_getDerivedPosition();
+
+		// apply scale
+		if (mApplyParentScale)
+		{
+			scale3x3[0][0] = mParentNode->_getDerivedScale().x / 2;
+			scale3x3[1][1] = mParentNode->_getDerivedScale().y / 2;
+			scale3x3[2][2] = mParentNode->_getDerivedScale().z / 2;
+		}
+		else
+		{
+			scale3x3[0][0] = 1;
+			scale3x3[1][1] = 1;
+			scale3x3[2][2] = 1;
+		}
+
+		// apply all transforms to xform       
+		*xform = (rot3x3 * scale3x3);
+		xform->setTrans(ppos);
+	}
+}
+
+void ProgressBar::getRenderOperation(Ogre::RenderOperation & op)
+{
+	if (isVisible())
+	{
+		if (mNeedUpdateGeometry)
+			_setupGeometry();
+
+		op = mRenderOp;
+	}
+}
+
+void ProgressBar::_notifyCurrentCamera(Ogre::Camera *cam)
+{
+	mpCam = cam;
+}
+
+void ProgressBar::_updateRenderQueue(Ogre::RenderQueue* queue)
+{
+	if (isVisible())
+	{
+		if (mNeedUpdateGeometry)
+			_setupGeometry();
+
+		queue->addRenderable(this, mRenderQueueID, OGRE_RENDERABLE_DEFAULT_PRIORITY);
+		//      queue->addRenderable(this, mRenderQueueID, RENDER_QUEUE_SKIES_LATE);
+	}
+}
+
+#if (OGRE_VERSION_MAJOR >= 1 && OGRE_VERSION_MINOR >= 6)
+void ProgressBar::visitRenderables(Renderable::Visitor* visitor, bool debugRenderables)
+{
+	visitor->visit(this, 0, false);
+}
+
+
+////////////////////// the Progress Bar with Text //////////////////////////////////////
+
+ProgressBarWithText::ProgressBarWithText(const Ogre::String & name, const Ogre::String & caption)
+: m_Bar(name + "_Bar"), m_Txt(name + "_BarText", caption, true, "BlueHighway", 10)
+{
+	mCaption = caption;
+	m_Txt.setTextAlignment(MovableText::H_CENTER, MovableText::V_ABOVE);
+}
+
+
+void ProgressBarWithText::setProgress(Real value)
+{
+	m_Bar.setProgress(value);
+	m_Txt.setCaption(mCaption + StringConverter::toString((int)(value*100)) +" %");
+}
+
+void ProgressBarWithText::attach(SceneNode *pNode)
+{
+	pNode->attachObject(&m_Bar);	
+	pNode->attachObject(&m_Txt);
+}
+
+void ProgressBarWithText::detach()
+{
+	m_Bar.detatchFromParent();
+	m_Txt.detatchFromParent();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ Sources/Navigator/navigatorModule/OgreGraphicObjects/ProgressBar.h
@@ -0,0 +1,156 @@
+/*
+This source file is part of Solipsis
+    (Solipsis is an opensource decentralized Metaverse platform)
+For the latest info, see http://www.solipsis.org/
+
+Copyright (C) 2006-2008 ANR-RIAM (IRISA, Archivideo, Artefacto, Rennes 2 University, Orange Labs)
+Author Christophe Floutier
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef __ProgressBar_h__
+#define __ProgressBar_h__
+
+#include "MovableText.h"
+
+using namespace Ogre;
+
+namespace Solipsis {
+
+/** This class represents an The sound icon shown above the avatar.
+*/
+class ProgressBar : public Ogre::MovableObject, public Ogre::Renderable
+{
+public:
+	ProgressBar(const Ogre::String & name, Real width = 200, Real height = 20, bool applyParentScale = true);
+	~ProgressBar();
+
+	// change the material
+	// default is 'Examples/ProgressBar'
+	// UV coordinate must be 0 0 1 0.5 for the front bar
+	// and 0.5 0 1 1 for the background
+	void setMaterial(const Ogre::String & materialName)
+	{
+		mNeedUpdateMaterial = true;
+		mMaterialName  = materialName;
+		mNeedUpdateGeometry = true;
+	}
+
+	void setSize(Real width = 200, Real height = 20)
+	{
+		mwidth = width;
+		mheight = height;
+		mNeedUpdateGeometry = true;
+	};
+
+	void setProgress(Real value);
+	Real getProgress() {return mProgress;}
+	void showOnTop(bool show);
+
+protected:
+	// from MovableText, create the object
+	void    _setupGeometry();
+	void	_setupMaterial();
+
+	// from MovableObject
+	void    getWorldTransforms(Ogre::Matrix4 *xform) const;
+	float   getBoundingRadius(void) const {return mRadius;};
+	float   getSquaredViewDepth(const Ogre::Camera *cam) const {return 0;};
+	const   Ogre::Quaternion        & getWorldOrientation(void) const;
+	const   Ogre::Vector3           & getWorldPosition(void) const;
+	const   Ogre::AxisAlignedBox    & getBoundingBox(void) const 
+	{
+		return mAABB;
+	};
+
+	const   Ogre::String            & getMovableType(void) const {static Ogre::String movTypeBar = "ProgressBar"; return movTypeBar;};
+
+	void    _notifyCurrentCamera(Ogre::Camera *cam);
+	void    _updateRenderQueue(Ogre::RenderQueue* queue);
+
+#if (OGRE_VERSION_MAJOR >= 1 && OGRE_VERSION_MINOR >= 6)
+	virtual void visitRenderables(Renderable::Visitor* visitor, bool debugRenderables);
+#endif
+
+	// from renderable
+	void    getRenderOperation(Ogre::RenderOperation &op);
+	const   Ogre::MaterialPtr       &getMaterial(void) const {assert(!mpMaterial.isNull());return mpMaterial;};
+	const   Ogre::LightList         &getLights(void) const {return mLList;};
+
+private:
+	float                   mRadius;
+
+	Ogre::RenderOperation   mRenderOp;
+	Ogre::AxisAlignedBox    mAABB;
+	Ogre::MaterialPtr		mpMaterial;
+	Ogre::LightList			mLList;
+	Ogre::Camera            *mpCam;
+
+	bool mNeedUpdateGeometry;
+	bool mNeedUpdateMaterial;
+	bool mApplyParentScale;
+
+	Real mwidth, mheight;
+	bool mOnTop;
+	Real mProgress;
+	Ogre::String mMaterialName;
+};
+
+class ProgressBarWithText
+{
+public:
+	ProgressBarWithText(const Ogre::String & name, const Ogre::String & caption);
+	~ProgressBarWithText();
+
+	void setBarMaterial(const Ogre::String & materialName) {m_Bar.setMaterial(materialName);}
+	void setBarSize(Real width = 200, Real height = 20) {m_Bar.setSize(width, height);}
+	void setCaption(const Ogre::String & caption) 
+	{
+		m_Txt.setCaption(caption); 
+		setProgress(m_Bar.getProgress());
+	}
+
+	void setFont(const Ogre::String & fontName, Real size, const ColourValue & color = ColourValue::White, unsigned int spacewidth = 2)
+	{
+		m_Txt.setFontName(fontName);
+		m_Txt.setCharacterHeight(size);
+		m_Txt.setColor(color);
+		m_Txt.setSpaceWidth(spacewidth);
+	}
+
+	void setTxtPosition(Real pos = 0)
+	{
+		m_Txt.setAdditionalHeight(pos);
+	}
+
+	void setProgress(Real value);
+
+	void attach(SceneNode *pNode);
+	void detach();
+	
+private:
+	Ogre::String mCaption; 
+
+	ProgressBar m_Bar;
+	MovableText m_Txt;
+};
+
+
+
+
+} // namespace Solipsis
+
+#endif // #ifndef __ProgressBar_h__
