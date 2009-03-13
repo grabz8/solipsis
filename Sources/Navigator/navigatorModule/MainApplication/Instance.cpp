72c1ccf16de63108c6670e778d49679e7737e327
Jangre (jangre) <>
2009-03-13 16:09:39 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1431 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Sources/Navigator/navigatorModule/MainApplication/Instance.cpp
+++ Sources/Navigator/navigatorModule/MainApplication/Instance.cpp
@@ -24,7 +24,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "Prerequisites.h"
 #include "Instance.h"
 #include "MainApplication/AutoCreatedWindow.h"
-#include "Platforms/Platform.h"
 #include "OgreTools/OgreHelpers.h"
 
 using namespace Solipsis;
