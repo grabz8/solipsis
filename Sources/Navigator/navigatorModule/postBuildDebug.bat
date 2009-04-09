02236ef74b34c82fff2e7cb89825d6f88eb959d1
Jangre (jangre) <>
2009-04-09 15:07:55 +0000

    skp plugin dependencies updated
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1549 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Sources/Navigator/navigatorModule/postBuildDebug.bat
+++ Sources/Navigator/navigatorModule/postBuildDebug.bat
@@ -73,7 +73,7 @@ if not exist "%dst%\NaviLocal\" xcopy "..\..\..\Media\NaviLocal\*.*" "%dst%\Navi
 if not exist "%dst%\lua\" xcopy "..\..\..\Media\lua\*.*" "%dst%\lua" /s /y /i
 if not exist "%dst%\fmodexL.dll" xcopy "%FMOD_PATH%\fmodexL.dll" "%dst%" /s /y
 if not exist "%dst%\plugin_3ds_d.dll" xcopy "..\..\..\Dependencies\plugin_3ds\lib\plugin_3ds_d.dll" "%dst%" /s /y
-if not exist "%dst%\plugin_skp_d.dll" xcopy "..\..\..\Dependencies\plugin_skp\include\Debug\*.dll" "%dst%" /s /y
+if not exist "%dst%\plugin_skp_d.dll" xcopy "..\..\..\Dependencies\plugin_skp\lib\Debug\*.dll" "%dst%" /s /y
 if not exist "%dst%\libcurld.dll" xcopy "..\..\..\Dependencies\curl\lib\DebugDLL\libcurld.dll" "%dst%" /s /y
 xcopy "..\..\..\Dependencies\navigatorConfigDialog\lib\WIN32\Debug\navigatorConfigDialog.dll" "%dst%" /s /y
 xcopy "..\..\..\Dependencies\VoiceEngine\lib\Debug\voiceEngine.dll" "%dst%" /s /y
