352f24b846db8f0d1976ad57efbc62af5fc6383a
vhuiban <>
2009-04-21 15:42:50 +0000

    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1588 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Sources/Modelers/TerrainModeler/TerrainGenerator.h
+++ Sources/Modelers/TerrainModeler/TerrainGenerator.h
@@ -34,7 +34,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 extern "C" {
 #define REAL float
 #define ANSI_DECLARATORS 1
-#include "triangle.h"
+#include <triangle.h>
 }
 
 namespace tege {
