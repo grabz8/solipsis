b564145b9971e75fc9e91badea1aa3d1c08a99ab
vhuiban <>
2009-05-04 09:09:34 +0000

    Added the copy of the declarative modeling cfg
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1640 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Sources/Navigator/navigatorModule/postBuildReleaseNCS.bat
+++ Sources/Navigator/navigatorModule/postBuildReleaseNCS.bat
@@ -89,6 +89,7 @@ if exist "%dst%\quake3settings.cfg" goto quake3settings_exist
  if exist "%OGRE_HOME%\bin\release\quake3settings.cfg" xcopy "%OGRE_HOME%\bin\release\quake3settings.cfg" "%dst%" /s /y
 :quake3settings_exist
 xcopy "..\..\..\Media\resources.cfg" "%dst%" /s /y
+xcopy "..\..\..\Media\declarativemodeling.cfg" "%dst%" /s /y
 
 call copyDocumentation.bat
 call copyNCSPhysXFiles.bat
