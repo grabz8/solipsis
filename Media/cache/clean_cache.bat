b2bc8cb3ae8e02c46b8c5938304d95bda8a91b81
Jangre (jangre) <>
2009-05-29 08:44:31 +0000

    Blank scene added
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/trunk@1676 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- Media/cache/clean_cache.bat
+++ Media/cache/clean_cache.bat
@@ -16,6 +16,7 @@ move 11112222_ref.xml 11112222_ref.xm_ >NUL
 move 11112223_ref.xml 11112223_ref.xm_ >NUL
 move 11112224_ref.xml 11112224_ref.xm_ >NUL
 move 11112235_ref.xml 11112235_ref.xm_ >NUL
+move 11112277_ref.xml 11112277_ref.xm_ >NUL
 move cache_ref.xml cache_ref.xm_ >NUL
 del *.sif 2>NUL
 del *.sof 2>NUL
@@ -28,6 +29,8 @@ move 11112224_ref.xm_ 11112224_ref.xml >NUL
 copy 11112224_ref.xml 11112224.xml >NUL
 move 11112235_ref.xm_ 11112235_ref.xml >NUL
 copy 11112235_ref.xml 11112235.xml >NUL
+move 11112277_ref.xm_ 11112277_ref.xml >NUL
+copy 11112277_ref.xml 11112277.xml >NUL
 move cache_ref.xm_ cache_ref.xml >NUL
 copy cache_ref.xml cache.xml >NUL
 
