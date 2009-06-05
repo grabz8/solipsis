ed84606ae8ac14ee06cabc0ad5d837a3a20e524e
Christophe Floutier (cfloutier) <cfloutier@gmail.com>
2009-06-05 15:53:59 +0000

    .....
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/solipsis/branches/Navi2.2@1698 53240bf5-9d7a-4d4b-845a-3b2958f72d47

--- DebugHelperParameters/copyToNavi.bat
+++ DebugHelperParameters/copyToNavi.bat
@@ -1,7 +1,5 @@
 echo off
-copy /y boot.lua ..\Common\bin\navigator\Release\lua\boot.lua
-copy /y boot.lua ..\Common\bin\navigator\Debug\lua\boot.lua
-
 copy /y uilogin.html ..\Common\bin\navigator\Release\NaviLocal\uilogin.html
 copy /y uilogin.html ..\Common\bin\navigator\Debug\NaviLocal\uilogin.html
+
 pause
